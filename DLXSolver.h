78d98ab67e3a2dc536d95058e37509d6ebab0652
Bruke <bruke.mammo@gmail.com>
2018-10-29 22:09:09 -0400

    board from string creation

---
+++ DLXSolver.h
@@ -0,0 +1,14 @@
+//
+// Created by bruke on 10/29/2018.
+//
+
+#ifndef PARALLELSUDOKU_DLXSOLVER_H
+#define PARALLELSUDOKU_DLXSOLVER_H
+
+
+class DLXSolver {
+
+};
+
+
+#endif //PARALLELSUDOKU_DLXSOLVER_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+#include "DLXSolver.h"
+
+#define N 9
+
+using namespace std;
+
+int ** fromString(string s);
+
+void printBoard(int ** board);
+
+void runExample();
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    runExample();
     return 0;
+}
+
+int ** fromString(string s){
+    //std::cout << s << std::endl;
+    int ** board = new int*[N];
+    for (int r = 0; r < N; r++){
+        board[r] = new int[N];
+        //std::cout << "row: " << r << std::endl;
+        for (int c = 0; c < N; c++){
+            //std::cout << "col: " << r << std::endl;
+            int index = c + (N * r);
+            //std::cout << "index: " << index << std::endl;
+            char ch = s[index];
+            //std::cout << "char: " << ch << std::endl;
+            if(ch != '.'){
+                board[r][c] = ch - '0';
+            } else {
+                board[r][c] = 0;
+            }
+        }
+    }
+    return board;
+}
+
+void printBoard(int ** board){
+    for (int r = 0; r < N; r++){
+        for (int c = 0; c < N; c++)
+            printf("%2d", board[r][c]);
+        printf("\n");
+    }
+}
+
+void runExample(){
+    vector<long> timings;
+
+    int ** board = fromString(const_cast<char *>("..5....3....9...8....57.....9.7....3.7.13..5.3.2......2...8......1..94259....78.."));
+    //printBoard(board);
+
+    //DLXSolver solver = new DLXSolver();
+
+
+
+//    for (auto i : timings){
+//        cout << i << endl;
+//    }
 }
