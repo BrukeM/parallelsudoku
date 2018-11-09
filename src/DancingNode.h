ce3c3ac50cfd55765f381f36504c53701c066bd8
praveern <pnarwelkar@gmail.com>
2018-11-08 22:41:47 -0500

    Removed implementations from header

--- src/DancingNode.h
+++ src/DancingNode.h
@@ -21,53 +21,17 @@ public:
     DancingNode* D;
     ColumnNode* C;
 
-    DancingNode* hookDown(DancingNode* n1){
-        assert(this->C == n1->C);
-        n1->D = this->D;
-        n1->D->U = n1;
-        n1->U = this;
-        this->D = n1;
-        return n1;
-    }
+    DancingNode* hookDown(DancingNode* n1);
 
-    DancingNode* hookRight(DancingNode* n1){
-        n1->R = this->R;
-        n1->R->L = n1;
-        n1->L = this;
-        this->R = n1;
-        return n1;
-    }
+    DancingNode* hookRight(DancingNode* n1);
 
-    void unlinkLR(){
-        this->L->R = this->R;
-        this->R->L = this->L;
-        updates++;
-    }
+    void unlinkLR();
+    void relinkLR();
+    void unlinkUD();
+    void relinkUD();
 
-    void relinkLR(){
-        this->L->R = this->R->L = this;
-        updates++;
-    }
-
-    void unlinkUD(){
-        this->U->D = this->D;
-        this->D->U = this->U;
-        updates++;
-    }
-
-    void relinkUD(){
-        this->U->D = this->D->U = this;
-        updates++;
-    }
-
-    DancingNode (){
-        L = R = U = D = this;
-    }
-
-    DancingNode (ColumnNode* c){
-        this();
-        C = c;
-    }
+    DancingNode();
+    DancingNode(ColumnNode* c);
 };
 
 #endif //PARALLELSUDOKU_DANCINGNODE_H
