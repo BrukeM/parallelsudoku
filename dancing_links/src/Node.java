e455d91c35f316b218ca5a06afbce60113236325
tahakapasi <t.s.m.kapasi@gmail.com>
2018-12-04 22:41:25 -0500

    first push

---
+++ dancing_links/src/Node.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author taha
+ */
+public class Node {
+    int xUp, yUp, xDown, yDown, xLeft, yLeft, xRight, yRight, xColumn, yColumn, size ;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getxUp() {
+        return xUp;
+    }
+
+    public void setxUp(int xUp) {
+        this.xUp = xUp;
+    }
+
+    public int getyUp() {
+        return yUp;
+    }
+
+    public void setyUp(int yUp) {
+        this.yUp = yUp;
+    }
+
+    public int getxDown() {
+        return xDown;
+    }
+
+    public void setxDown(int xDown) {
+        this.xDown = xDown;
+    }
+
+    public int getyDown() {
+        return yDown;
+    }
+
+    public void setyDown(int yDown) {
+        this.yDown = yDown;
+    }
+
+    public int getxLeft() {
+        return xLeft;
+    }
+
+    public void setxLeft(int xLeft) {
+        this.xLeft = xLeft;
+    }
+
+    public int getyLeft() {
+        return yLeft;
+    }
+
+    public void setyLeft(int yLeft) {
+        this.yLeft = yLeft;
+    }
+
+    public int getxRight() {
+        return xRight;
+    }
+
+    public void setxRight(int xRight) {
+        this.xRight = xRight;
+    }
+
+    public int getyRight() {
+        return yRight;
+    }
+
+    public void setyRight(int yRight) {
+        this.yRight = yRight;
+    }
+
+    public int getxColumn() {
+        return xColumn;
+    }
+
+    public void setxColumn(int xColumn) {
+        this.xColumn = xColumn;
+    }
+
+    public int getyColumn() {
+        return yColumn;
+    }
+
+    public void setyColumn(int yColumn) {
+        this.yColumn = yColumn;
+    }
+}
